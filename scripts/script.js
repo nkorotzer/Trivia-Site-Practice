114ea7bc3301dae54c55cced8c8f19413e69ff21
Nick Korotzer <nick.korotzer@gmail.com>
2025-07-29 09:05:14 +0900

    Added whitespace to query string

--- scripts/script.js
+++ scripts/script.js
@@ -40,19 +40,19 @@ function searchAnimeByID (id) {
     // Storing it in a separate .graphql/.gql file is also possible
     var query = `
     query ($id: Int) { # Define which variables will be used in the query (id)
-    Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
-        id
-        coverImage {
-            medium
+        Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+            id
+            coverImage {
+                medium
+            }
+            title {
+            romaji
+            english
+            native
+            }
+            season
+            seasonYear
         }
-        title {
-        romaji
-        english
-        native
-        }
-        season
-        seasonYear
-    }
     }
     `;
 
